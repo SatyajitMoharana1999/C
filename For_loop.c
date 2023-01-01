689cb24137eed68d9b910a16d11571e1897e5324
Satyajit Moharana <117591813+SatyajitMoharana1999@users.noreply.github.com>
2023-01-01 17:32:52 +0530

    Create For_loop.c

---
+++ For_loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  int i;
+  int n;
+  printf("enter your number:");
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+  printf("%d",i);
+  printf("\n");
+  }
+  return 0;
+}
