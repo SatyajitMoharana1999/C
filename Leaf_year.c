dfa0b824870fed43edcc4140e13509f97cf098f5
Satyajit Moharana <117591813+SatyajitMoharana1999@users.noreply.github.com>
2023-01-04 08:18:23 +0530

    Update Leaf_year.c

--- Leaf_year.c
+++ Leaf_year.c
@@ -1,12 +1,25 @@
-#includr<stdio.h>
-int main{
+#include<stdio.h>
+
+int main(){
+
   int year;
-  scanf("%d",year);
+
+  printf("enter year");
+
+  scanf("%d",&year);
+
   if(year%4==0){
+
     printf("%d is a leaf year",year);
+
    }
+
   else{
+
     printf("%d is not a leaf year",year);
+
    }
+
   return 0;
+
 }
