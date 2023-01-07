70115645c1596bcf6df2cf2cbd8a334431f790aa
Satyajit Moharana <117591813+SatyajitMoharana1999@users.noreply.github.com>
2023-01-07 13:20:59 +0530

    Create Calculating_the_area_of_circle.c

---
+++ Calculating_the_area_of_circle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+  float radius;
+  float area=3.141*radius*radius;
+  printf("Enter the radius of the Circle");
+  scanf("%f",&radius);
+  printf("Area of the Circle is %d",area);
+  return 0;
+}
