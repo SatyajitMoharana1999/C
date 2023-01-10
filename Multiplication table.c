5792fe5215abd7f9746897ee73539d33e57a6aa7
Satyajit Moharana <117591813+SatyajitMoharana1999@users.noreply.github.com>
2023-01-10 09:32:44 +0530

    Update Multiplication table.c

--- Multiplication table.c
+++ Multiplication table.c
@@ -5,7 +5,7 @@ int main(){
   printf("Enter which multiplication table you want to see");
   scanf("%d",&input);
   for(int i=1;i<=10;i++){
-    printf("%d * %d = %d",input,i,input*i);
+    printf("%d * %d = %d\n",input,i,input*i);
     
   }
 }
