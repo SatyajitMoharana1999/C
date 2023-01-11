2190c7ca37b113b39189023e3d2b4e9a37eeb292
Satyajit Moharana <117591813+SatyajitMoharana1999@users.noreply.github.com>
2023-01-11 13:13:42 +0530

    Update Reverse number.c

--- Reverse number.c
+++ Reverse number.c
@@ -5,8 +5,8 @@ int main(){
   int reverse=0;
   printf("Enter any number");
   scanf("%d",&input);
-  for(i=input;i>0;i=i/10){
-    rem=i%10;
+  for(int i=input;i>0;i=i/10){
+    reminder=i%10;
     reverse=reverse*10+reminder;
   }
   printf("%d",reverse);
