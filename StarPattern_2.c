03ecd67cd25d300045ee71107fc618fddb83a136
Satyajit Moharana <117591813+SatyajitMoharana1999@users.noreply.github.com>
2023-01-18 13:15:35 +0530

    Update StarPattern_2.c

--- StarPattern_2.c
+++ StarPattern_2.c
@@ -1,10 +1,10 @@
-#include<studio.h>
+#include<stdio.h>
 int main(){
   int n;
   printf("Enter a number");
-  scanf("%d",&a);
+  scanf("%d",&n);
   for(int i=1;i<=n;i++){
-    for(int j=1;j<n;j++){
+    for(int j=1;j<=n-i;j++){
       printf(" ");
     }
     for(int k=1;k<=2*i-1;k++){
